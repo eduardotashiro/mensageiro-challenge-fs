94b9b7e3eaee570eefb28065ff3cacb5b0c182c1
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-02-21 23:28:30 -0300

    feat: return message ivalid email in schema

--- backend/src/services/registerService.ts
+++ backend/src/services/registerService.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 
 
 const createUserSchema = z.object({
-    email: z.email(),
+    email: z.email({ message: "Invalid email address !" }),
     password: z.string().min(8, { message: "Password must be at least 8 characters long !" }),
     name: z.string().min(1, { message: "Name is required !" }),
 });
