708126dffe3c0a0e090f3ae45b84331db527584f
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-02-23 23:05:47 -0300

    feat: backend integration for register [ok]

--- frontend/src/scripts/register.js
+++ frontend/src/scripts/register.js
@@ -11,68 +11,68 @@ form.addEventListener("submit", (event) => {  // Adiciona um ouvinte de evento n
 
 })
 
-email.addEventListener("blur", () =>{   // Quando o usuário sai do campo de email (evento "blur"), chama a validação do email
+email.addEventListener("blur", () => {   // Quando o usuário sai do campo de email (evento "blur"), chama a validação do email
     checkInputEmail();
 })
 
-username.addEventListener("blur", () =>{
+username.addEventListener("blur", () => {
     checkInputUsername();
 })
 
 
- 
-function checkInputUsername(){  // Validação do campo username (nome de usuário)
+
+function checkInputUsername() {  // Validação do campo username (nome de usuário)
     const usernameValue = username.value;  //pega o valor digitado no input
 
-   if(usernameValue === ""){
-     // Se estiver vazio, mostra erro
-   errorInput(username, "O nome de usuário é obrigatório")
-   }else{
-    // Se estiver ok, remove classe de erro (mantém classe normal)
-    const formItem = username.parentElement;
-    formItem.className = "form-content"
-   }
-}
-
-function checkInputEmail(){ // Validação do campo de email
-    const emailValue = email.value;
-    if(emailValue ===""){
-          // Se estiver vazio, mostra erro
-        errorInput(email, "O email é obrigatório")
-    }else{  
-         // Se estiver preenchido, remove erro
-    const formItem = email.parentElement;
-    formItem.className= "form-content"
-        
+    if (usernameValue === "") {
+        // Se estiver vazio, mostra erro
+        errorInput(username, "O nome de usuário é obrigatório")
+    } else {
+        // Se estiver ok, remove classe de erro (mantém classe normal)
+        const formItem = username.parentElement;
+        formItem.className = "form-content"
     }
 }
 
-function checkInputPassword(){  // Validação do campo de senha
+function checkInputEmail() { // Validação do campo de email
+    const emailValue = email.value;
+    if (emailValue === "") {
+        // Se estiver vazio, mostra erro
+        errorInput(email, "O email é obrigatório")
+    } else {
+        // Se estiver preenchido, remove erro
+        const formItem = email.parentElement;
+        formItem.className = "form-content"
+
+    }
+}
+
+function checkInputPassword() {  // Validação do campo de senha
     const passwordValue = password.value;
 
-    if(passwordValue ===""){
+    if (passwordValue === "") {
         errorInput(password, "A senha é obrigatória")
-    }else if(passwordValue.length < 8){   // Se tiver menos de 8 caracteres, mostra outro erro
+    } else if (passwordValue.length < 8) {   // Se tiver menos de 8 caracteres, mostra outro erro
         errorInput(password, "A senha precisa ter no mínimo 8 caracteres")
-    }else{
+    } else {
         const formItem = password.parentElement;    // Se estiver tudo certo, remove classe de erro
-        formItem.className = "form-content "
-     }   
-    
+        formItem.className = "form-content"
     }
 
-    function checkInputPasswordConfirmation(){  // Validação do campo de confirmação de senha
+}
+
+function checkInputPasswordConfirmation() {  // Validação do campo de confirmação de senha
     const passwordValue = password.value;
     const confirmationPasswordValue = passwordConfirmation.value
 
-    if(passwordValue ===""){  
-          // Se o campo senha estiver vazio, exibe erro na confirmação também
+    if (passwordValue === "") {
+        // Se o campo senha estiver vazio, exibe erro na confirmação também
         errorInput(passwordConfirmation, "A confirmação de senha é obrigatória.")
-    }else if(confirmationPasswordValue !== passwordValue){
-         // Se as senhas forem diferentes, mostra erro
+    } else if (confirmationPasswordValue !== passwordValue) {
+        // Se as senhas forem diferentes, mostra erro
         errorInput(passwordConfirmation, "As senhas não são iguais")
-    }else{
-         // Se tudo estiver correto, remove erro
+    } else {
+        // Se tudo estiver correto, remove erro
         const formItem = passwordConfirmation.parentElement;
         formItem.className = "form-content"
 
@@ -80,32 +80,55 @@ function checkInputPassword(){  // Validação do campo de senha
 }
 
 
-function checkForm(){  // Valida todos os campos do formulário
-    
+async function checkForm() {  // Valida todos os campos do formulário
+    console.log("checkForm chamado!"); // ← adiciona isso
+
     checkInputUsername();
     checkInputEmail();
     checkInputPassword();
     checkInputPasswordConfirmation();
-         
+
     const formItems = form.querySelectorAll(".form-content")    // Pega todos os elementos com a classe form-content
 
-    const isValid = [...formItems].every( (item) => {  // Verifica se todos os elementos têm apenas a classe "form-content"
+    const isValid = [...formItems].every((item) => {  // Verifica se todos os elementos têm apenas a classe "form-content"
         return item.className === "form-content"
-
     });
 
-    if(isValid){    // Se todos os campos forem válidos, mostra mensagem de sucesso
-       const userData = {
-        username: username.value,
-        email: email.value,
-        password: password.value
-       }
-      //PAREI AQUI ***************************************
+    if (isValid) {    // Se todos os campos estiverem validos redireciono para  a página de login 
+        try {
+            console.log("Formulário válido, enviando dados para o backend...")
+            const userData = {
+                name: username.value,
+                email: email.value,
+                password: password.value
+            }
+
+            const response = await fetch("http://localhost:3000/api/auth/register", {  // Envia os dados do usuário para o backend (rota de criação de usuário)
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(userData)
+            })
+
+            if (!response.ok) {  // Se a resposta do backend for ok, redireciona para a página de login
+                throw new Error("Erro ao criar usuário")
+            }
+            await response.json();
+
+            alert("Usuário criado com sucesso! Agora você pode fazer login.");
+
+            window.location.href = "login.html"; // Redireciona para a página de login
+
+        } catch (error) {
+            alert("Erro ao criar usuário: ")
+            console.log(error)
+        }
     }
 }
-    
 
-function errorInput(input, message){  // Exibe o erro visual no campo
+
+function errorInput(input, message) {  // Exibe o erro visual no campo
 
     const formItem = input.parentElement; // pega a div pai (wrapper do input)
     const textMessage = formItem.querySelector("a") // pega o elemento que mostrará a mensagem (tagg <a>)
